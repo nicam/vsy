bd1aceb544e9be1047c8a24c8ac0d8decead3a56
Adrian Bertschi <adrian.bertschi+github@gmail.com>
2014-04-15 14:58:34 +0200

    blubber

--- P3/src/ch/zhaw/client/Environment.java
+++ P3/src/ch/zhaw/client/Environment.java
@@ -1,6 +1,9 @@
 package ch.zhaw.client;
 
 public abstract class Environment {
-    public static String host = "188.40.90.140";
+    //public static String host = "188.40.90.140";
+    //public static int port = 1234;
+
+    public static String host = "localhost";
     public static int port = 1234;
 }
