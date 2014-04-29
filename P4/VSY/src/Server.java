50cf35c0795d296624d10a53e5d4e4ee31f7a043
Pascal Helfenstein <pascal.helfenstein@gmail.com>
2014-04-29 14:09:56 +0200

    rename stuff

--- P4/VSY/src/Server.java
+++ P4/VSY/src/Server.java
@@ -2,11 +2,12 @@ import java.io.IOException;
 
 import com.sun.jersey.api.container.httpserver.HttpServerFactory;
 
-public class Test {
+public class Server {
 	
+	@SuppressWarnings("restriction")
 	public static void main(String[] args) {
 		try {
-			HttpServerFactory.create( "http://localhost:8080/").start();
+			HttpServerFactory.create("http://localhost:8080/").start();
 		} catch (IllegalArgumentException e1) {
 			e1.printStackTrace();
 		} catch (IOException e1) {
