e31efb9a943b5696874e651b4f89531c810f9c9d
Adrian Bertschi <adrian.bertschi+github@gmail.com>
2014-05-12 14:34:55 +0200

    new stuff

--- P5/certs/makeCerts.sh
+++ P5/certs/makeCerts.sh
@@ -1,14 +1,10 @@
+# Generate server certificate
 keytool -genkeypair -alias serverkey -keyalg RSA -dname "CN=Web Server,OU=Application Development,O=ZHAW,L=Winterthur,S=ZH,C=CH" -keypass password -storepass password -keystore server.jks
-keytool -genkeypair -alias clientkey -keyalg RSA -dname "CN=Client,OU=Application Development,O=ZHAw,L=Winterthur,S=ZH,C=CH" -keypass password -storepass password -keystore client.jks
+keytool -importcert -keystore server.jks -alias servercert -file server-public.cer -storepass password -noprompt
 
-keytool -exportcert -alias clientkey -file client-public.cer -keystore client.jks -storepass password
-keytool -importcert -keystore server.jks -alias clientcert -file client-public.cer -storepass password -noprompt
- 
-# view the contents of the keystore (use -v for verbose output)
-keytool -list -keystore server.jks -storepass password
-
-keytool -exportcert -alias serverkey -file server-public.cer -keystore server.jks -storepass password
-keytool -importcert -keystore client.jks -alias servercert -file server-public.cer -storepass password -noprompt
- 
-# view the contents of the keystore (use -v for verbose output)
-keytool -list -keystore client.jks -storepass password
+# generate client certificate
+keytool -genkey -v -alias clientKey -keyalg RSA -storetype PKCS12 -keystore client.p12 -dname "CN=Client,OU=Application Development,O=ZHAW,L=Winterthur,S=ZH,C=CH" -storepass password -keypass password
+# export client certificate 
+keytool -export -alias clientKey -keystore client.p12 -storetype PKCS12 -storepass password -rfc -file client.cer
+# import client certificate into server keystore
+keytool -import -v -file client.cer -keystore server.jks -storepass password
